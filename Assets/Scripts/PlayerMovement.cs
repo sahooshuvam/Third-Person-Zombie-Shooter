304b7fca79ee481eb6c187573fb81e5988f38d8a
Shuvam Sahoo <shuvam.sahoo@yesgnome.com>
2022-04-30 20:59:29 +0530

    PlayerMovement Scripts And Cinemachine is Added...

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,21 +4,37 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public CharacterController characterController;
+    private CharacterController characterController;
+    private Animator animator;
+    [SerializeField] Transform camTransform;
+    [SerializeField] float playerSpeed;
+    [SerializeField] float turnSmoothTime = 0.1f;
+    [SerializeField] float turnSmoothVelocity;
+
 
     private void Awake()
     {
-        characterController = this.GetComponent<CharacterController>();
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontal,0f,vertical);
+
+        if (direction.magnitude >0f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camTransform.eulerAngles.y;//
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);//
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f,targetAngle,0f) * Vector3.forward;
+            characterController.Move(moveDir.normalized *playerSpeed *Time.deltaTime);
+            animator.SetFloat("Speed",direction.magnitude);
+        }
     }
 }
